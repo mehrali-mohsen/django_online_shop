f00a4588bd4102421581879bc96ce1e27ee4a54f
mohsen <mehralimohsen1370@gmail.com>
2025-02-16 10:36:41 +0330

    use ckeditor as rich text editor for product description

--- products/models.py
+++ products/models.py
@@ -4,9 +4,11 @@ from django.shortcuts import reverse
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 
+from ckeditor.fields import RichTextField
+
 class Product(models.Model):
     title = models.CharField(max_length=100, verbose_name=_('title'))
-    description = models.TextField(verbose_name=_('description'))
+    description = RichTextField(verbose_name=_('description'))
     price = models.PositiveIntegerField(default=0, verbose_name=_('price'))
     active = models.BooleanField(default=True, verbose_name=_('active'))
     image = models.ImageField(verbose_name=_('Product Image'), upload_to='product/product_cover/', blank=True, )
